a6c0251facc06a6bcf0117b28b3330714de5b634
Barzizzag <Guillermobarzizza@hotmail.com>
2023-06-22 16:40:42 -0300

    Add files via upload
    Merge registro turnos

---
+++ registroturnos.php
@@ -0,0 +1,64 @@
+<?php 
+session_start();
+include('header.php');
+include('conexion.php');
+$date ="'".date('Y-m-d')."'";
+$new_query = "SELECT turnos.idturno, turnos.fecha, turnos.time, turnos.idservicio, servicios.tiposervicio, turnos.cod_area, turnos.telefono, 
+                turnos.mail, usuarios.nombre, usuarios.apellido FROM turnos, usuarios,servicios 
+                WHERE turnos.mail = usuarios.mail AND turnos.idservicio=servicios.idservicio AND turnos.fecha = $date ORDER BY turnos.time ASC";
+$query = $conn->query($new_query);
+
+?>
+<section class="first main-font container-fluid text-white d-flex justify-content-center">
+
+    <div class="container px-4 col-12 col-md-10 col-lg-10 col-xl-10">
+        <h3 class="mt-4 text-center">Turnos del 
+            <?php
+                echo date('Y-m-d');
+            ?>
+        </h3>    
+        <div>
+            <table class="table table-success table-striped">
+                <thead>
+                    <td>Id</td>
+                    <td>Hora</td>
+                    <td>Email</td>
+                    <td>Nombre</td>
+                    <td>Apellido</td>
+                    <td>Servicio</td>
+                    <td>Codigo de area</td>
+                    <td>Telefono</td>
+                    <td></td>
+                </thead>
+                <tbody>
+                    <?php
+                        while ($fila = mysqli_fetch_array($query)){
+                            echo "<tr>
+                                <td>".$fila['idturno']."</td>
+                                <td>".$fila['time']."</td>
+                                <td>".$fila['mail']."</td>
+                                <td>".$fila['nombre']."</td>
+                                <td>".$fila['apellido']."</td>
+                                <td>".$fila['tiposervicio']."</td>
+                                <td>".$fila['cod_area']."</td>
+                                <td>".$fila['telefono'].'</td>
+                                <td><div class="d-flex justify-content-center bg-dark text-white">
+                                        <a href="#" class="mx-2 link-primary bg-dark"><i class="bi bi-pencil-fill bg-dark"></i></a>
+                                        <a href="#" class="mx-2 link-danger bg-dark"><i class="bi bi-trash icon bg-dark"></i></a>
+                                    </div>
+                                </td>
+                            </tr>';
+                        }
+                    ?>  
+                </tbody>
+                
+            </table>
+        </div>
+    </div>
+</section>
+<?php include('footer.php');?>
+
+<!-- 
+//while ($registro = $query -> fetch(PDO::FETCH_ASSOC)){
+    //echo "ID: ".$registro["id_usuarios"] . "Usuarios: " . $registro["usuarios"] . "Password: ". $registro["password"] . "Passh: " . $registro["hash_password"] . "Email: " . $registro["mail"] . "Perfil: " . $registro["perfil"] . "Comentairos: " . $registro["comentarios_usuarios"] . "Estado: " . $registro["estado"] . "Fecha: " . $registro["fecha_usuario"] . "<br>";
+//}-->
